9659a48b035ef3428e584961217443c9a7397a8a
muminmooo <91783523+muminmooo@users.noreply.github.com>
2021-10-03 18:23:09 +0700

    Update app.py

--- app.py
+++ app.py
@@ -26,7 +26,7 @@ UPLOAD_FOLDER ='static/uploads/'
 DOWNLOAD_FOLDER = 'static/downloads/'
 ALLOWED_EXTENSIONS = {'jpg', 'png','.jpeg'}
 
-lineaccesstoken = ''
+lineaccesstoken = 'NqC7IEBRVVivuAFDf8kuUBxdvUCqY7ZkueRPqGbKLq5u/9uBuPEkFbxPpKerIgKpCvN1niILndgpUGm1UKLFb0kA5d4KyG9vj+KpX6PcUcCPwcvnKoKHrdoni5iqwQP+54Dw0AByRcxU66DDvfwzaAdB04t89/1O/w1cDnyilFU='
 
 line_bot_api = LineBotApi(lineaccesstoken)
 
